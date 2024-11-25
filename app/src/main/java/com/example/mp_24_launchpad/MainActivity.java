b2d3664d50f59430401bacf098ac2f9b002e5c59
chiwoo <skcmemformlf0@gmail.com>
2024-11-26 03:11:56 +0900

    [이치우] Background 수정

--- app/src/main/java/com/example/mp_24_launchpad/MainActivity.java
+++ app/src/main/java/com/example/mp_24_launchpad/MainActivity.java
@@ -1,11 +1,14 @@
 package com.example.mp_24_launchpad;
 
 import android.os.Bundle;
+import android.widget.Toast;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.constraintlayout.widget.ConstraintLayout;
 import androidx.navigation.NavController;
+import androidx.navigation.NavDestination;
 import androidx.navigation.Navigation;
 import androidx.navigation.ui.AppBarConfiguration;
 import androidx.navigation.ui.NavigationUI;
@@ -15,6 +18,7 @@ import com.example.mp_24_launchpad.databinding.ActivityMainBinding;
 public class MainActivity extends AppCompatActivity {
 
     private ActivityMainBinding binding;
+    private ConstraintLayout container;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -29,6 +33,23 @@ public class MainActivity extends AppCompatActivity {
                 .build();
         NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_activity_main);
         NavigationUI.setupWithNavController(binding.navView, navController);
+        NavDestination currentDestination = navController.getCurrentDestination();
+
+        container = (ConstraintLayout) findViewById(R.id.container);
+
+        navController.addOnDestinationChangedListener((controller, destination, arguments) -> {
+            int currentId = destination.getId();
+
+            if (currentId == R.id.navigation_guitar) {
+                container.setBackgroundResource(R.drawable.guitar_background);
+            } else if (currentId == R.id.navigation_drum) {
+                container.setBackgroundResource(R.drawable.drum_background);
+            } else if (currentId == R.id.navigation_piano) {
+                Toast.makeText(getApplicationContext(), "현재 Fragment는 PianoFragment입니다.", Toast.LENGTH_SHORT).show();
+            } else {
+                System.out.println("알 수 없는 Fragment입니다.");
+            }
+        });
     }
 
 }
